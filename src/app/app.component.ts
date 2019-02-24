1631457efa8b56cccff5fe11d8f066ac982f73a9
datsgawas <dattaram.gawas@metamagic.in>
2019-02-24 12:31:09 +0530

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,68 @@
-import { Component } from '@angular/core';
+import {
+  Compiler, Component, Injector, NgModuleFactoryLoader, SystemJsNgModuleLoader, ViewChild, ViewContainerRef
+} from '@angular/core';
+
+declare const System;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
+  providers: [
+    {
+      provide: NgModuleFactoryLoader,
+      useClass: SystemJsNgModuleLoader
+    }
+  ],
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'module-generator';
+
+  @ViewChild('vc', {read: ViewContainerRef}) _container: ViewContainerRef;
+
+  data: any;
+
+  show: boolean = false;
+
+  url = './profile.module';
+
+  constructor(private _compiler: Compiler, private _injector: Injector) {
+this.data = {
+     url: './profile.module.js',
+    className: 'ProfileModule'
+  };
+
+
+  }
+
+  addModule() {
+      System.import(this.data.url).then((module) => {
+        this._compiler.compileModuleAndAllComponentsAsync(module[this.data.className])
+          .then((compiled) => {
+            const factory = compiled.componentFactories[0];
+            this._container.createComponent(factory);
+          });
+      });
+
+  }
+
+
+
+ /* addModule2() {
+    var loadScriptAsync = function (uri) {
+      return new Promise((resolve, reject) => {
+        var tag = document.createElement('script');
+        tag.src = uri;
+        tag.onload = () => {
+          resolve();
+        };
+        var firstScriptTag = document.getElementsByTagName('head')[0];
+        firstScriptTag.append(tag);
+      });
+    }
+    let scriptLoaded = loadScriptAsync('http://localhost/test/profile.module.js');
+    scriptLoaded.then((module)=>{
+      debugger;
+    })
+  }*/
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {RouterModule, Routes} from "@angular/router";
+
+/*const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '',
+    pathMatch: 'full'
+  },
+  {
+    path: 'profile', loadChildren: './profile.module#ProfileModule'
+  }
+];*/
 
 @NgModule({
   declarations: [
